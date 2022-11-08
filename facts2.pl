af947d539214d0094a30b6f8e2df65177a2b76d8
Divyaparadkar@007 <114057036+divgitp@users.noreply.github.com>
2022-11-08 21:18:22 +0530

    Add files via upload

---
+++ facts2.pl
@@ -0,0 +1,8 @@
+lectures(robin,course).
+studies(student,course).
+subjects(maths, hindi , physics).
+like(rohan,english).
+like(anu,maths).
+like(div, coding).
+like(pro,maths ,hindi, english, coding).
+
