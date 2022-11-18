38a8e005d84b089236f51506e23c46cf1edb0be8
Divyaparadkar@007 <114057036+divgitp@users.noreply.github.com>
2022-11-18 23:54:55 +0530

    Add files via upload

---
+++ list_2.pl
@@ -0,0 +1,7 @@
+thesaurus('like','love').
+thesaurus('like','fond').
+thesaurus('like','similar').
+thesaurus('like','akin').
+thasaueus('give','offer').
+thasaueus('give','bestow').
+thasaueus('give','present').
