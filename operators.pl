713c71850eb148c93695d2e3a4958f954b671180
Divyaparadkar@007 <114057036+divgitp@users.noreply.github.com>
2022-11-23 23:49:27 +0530

    Add files via upload

---
+++ operators.pl
@@ -0,0 +1,7 @@
+calc:- X is 100 +200 , write('100 + 200 is '),write(X),n1,
+       Y is 400 -400 , write('400 - 150 is') ,write(Y),n1,
+       Z is 10 * 300 , write('10 * 300 is'),write(Z),n1,
+       A is 100 / 30 , write('100 / 30 is'),write(A),n1,
+       B is 100 //30 , write('100 // 30 is'),write(B),n1,
+       C is 100 ** 2 , write('100 **2 is'),write(C),n1,
+       D is 100 mod30, write('100 mod 30 is '),write(D),n1.
